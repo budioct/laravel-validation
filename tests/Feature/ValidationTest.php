32c3d97e8ed5d232878599f3944c479a8ab97c15
Budhi Octaviansyah <budioct@gmail.com>
2024-06-27 20:01:57 +0700

    menjalankan Validator (validation)

--- tests/Feature/ValidationTest.php
+++ tests/Feature/ValidationTest.php
@@ -38,4 +38,61 @@ class ValidationTest extends TestCase
 
     }
 
+    /**
+     * Menjalankan Validasi
+     * ● Setelah kita membuat Validator, selanjutnya yang biasa kita lakukan adalah mengecek apakah
+     *   validasi sukses atau gagal
+     * ● Untuk melakukan itu, kita bisa menggunakan dua method yang mengembalikan nilai boolean
+     * ● fails(), akan mengembalikan true jika gagal, false jika sukses
+     * ● passes(), akan mengembalikan true jika sukses, false jika gagal
+     *
+     * note:
+     * fails() // Tentukan apakah data gagal dalam aturan validasi...
+     * passes() // Tentukan apakah data lolos aturan validasi...
+     */
+
+    public function testValidatorSuccess(){
+
+        $data = [
+            "username" => "admin",
+            "password" => "rahasia",
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        $validator = Validator::make($data, $rules); // Illuminate/Support/Facades/Validator // package validation untuk laravel
+
+        self::assertFalse($validator->fails()); // fails() // true jika gagal, false jika sukses
+        self::assertTrue($validator->passes()); // passes() // true jika sukses, false jika gagal
+
+        var_dump($validator->fails()); // bool(false)
+        var_dump($validator->passes()); // bool(true)
+
+    }
+
+    public function testValidatorFailed(){
+
+        $data = [
+            "username" => "",
+            "password" => "",
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        $validator = Validator::make($data, $rules); // Illuminate/Support/Facades/Validator // package validation untuk laravel
+
+        self::assertTrue($validator->fails()); // fails() // true jika gagal, false jika sukses
+        self::assertFalse($validator->passes()); // passes() // true jika sukses, false jika gagal
+
+        var_dump($validator->fails()); // bool(true)
+        var_dump($validator->passes()); // bool(false)
+
+    }
+
 }
